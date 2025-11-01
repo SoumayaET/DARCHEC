5a709aae3ce837970e528f65e4ea91df44a10895
Soumaya <et.Soumaya2@gmail.com>
2025-11-01 14:13:55 +0100

    Création du composant Recommended avec boutons de filtrage pour les catégories

--- src/produits-page/recommended/recommended.js
+++ src/produits-page/recommended/recommended.js
@@ -1,15 +1,15 @@
 import './recommended.css'
-
-export default function Recommended() {
+import Buttons from '../components/buttons'
+export default function Recommended({handleClick}) {
     return (
         <>
         <div>
             <h2 className='recommended-title'>Recommended</h2>
             <div className="recommended-flex">
-                <button className="btns">All produit</button>
-                <button className="btns">Femme</button>
-                <button className="btns">Homme</button>
-                
+                <button className='btns'>Tout</button>
+                <Buttons onClickHandler={handleClick} value="femme" title="Femme" />
+                <Buttons onClickHandler={handleClick} value="homme" title="Homme" />
+
             </div>
         </div>
         </>
